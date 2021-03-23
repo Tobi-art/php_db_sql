2c55eb8ff88b598825400b8ac1c967e682f1ed6d
Tobias <tobi.braendle@gmail.com>
2021-03-23 19:50:49 +0900

    search function and CSS problem fixed

--- update.php
+++ update.php
@@ -25,7 +25,7 @@ if ($status == false) {
     <link rel="stylesheet" href="CSS/reset.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/style2.css">
 </head>
 
 <body>
